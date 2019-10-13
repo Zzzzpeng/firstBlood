17adb9147be8a2289fd04d02ac1465c82f8a46ce
chenzepeng001 <2788281323@qq.com>
2019-10-13 17:59:37 +0800

    修改Mydev---测试权限

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,5 +1,5 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello-git-dev-guest");
+        System.out.println("hello-git-dev-guest"); //add by guest
     }
 }
